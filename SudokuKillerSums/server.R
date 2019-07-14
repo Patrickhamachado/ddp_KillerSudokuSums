77c8a0d184a0369948e86d7da5b07ea875b9e4c4
Patrickhamachado <patrickhamachado@gmail.com>
2019-07-14 17:44:47 -0500

    App UI migrated to shinymaterial

--- SudokuKillerSums/server.R
+++ SudokuKillerSums/server.R
@@ -1,49 +1,50 @@
 
 library(shiny)
+library(shinymaterial)
 
-# Funcion posibles --------------------------------------------------------
+# Function posibles --------------------------------------------------------
 
 posibles <- function( suma, digitos, excluir = NULL,
                       incluir = NULL ) {
-
+    
     nueve = 1:9
-
+    
     # Excluir
     # Los recibidos en excluir
     nueve = nueve[ !(nueve %in% excluir) ]
-
+    
     # Calcular
     ret = caTools::combs( nueve, digitos)
     sumas = rowSums( ret ) == suma
     ret = matrix( ret[ sumas,  ], nrow = sum( sumas ) )
     # ret = ret[ sumas,  ]
-
+    
     # Si no hay resultados, salir
     if ( nrow( ret ) == 0 ) {
-        print( "No hay opciones posibles!" )
-        return( ret )
+        # print( "No hay opciones posibles!" )
+        return( "None" )
     }
-
-
+    
+    
     # Incluir
     # Filtrar por lo recibido en incluir
     incluye = apply(ret, 1, function( fila )
     { all( incluir %in% fila ) } )
-
+    
     ret = matrix( ret[ incluye,  ], nrow = sum( incluye ) )
-
+    
     # Si no hay resultados, salir
     if ( nrow( ret ) == 0 ) {
-        print( "No hay opciones posibles!" )
-        return( ret )
+        # print( "No hay opciones posibles!" )
+        return( "None" )
     } else {
         return( ret )
     }
-
-
+    
+    
     # Imprimir !
-
-
+    
+    
     # No hay opciones
     if( length( ret ) > 1 ) {
         if( !is.null(nrow( ret ))  ) {
@@ -52,28 +53,116 @@ posibles <- function( suma, digitos, excluir = NULL,
         } else {
             paste0( ret, collapse = "")
         }
-
-
+        
+        
     } else {
-        print( "No hay opciones posibles" )
+        # print( "No hay opciones posibles!" )
+        return( "None" )
     }
-
+    
     # Imprimir opciones
     return( ret )
-
+    
 }
+
 
 
 # Shiny server ------------------------------------------------------------
 
-shinyServer(function(input, output) {
-
+shinyServer(function(input, output, session) {
+    
+    excluir.m <- reactive({
+        excluir <- c(input$exclude_1, input$exclude_2,
+                     input$exclude_3, input$exclude_4,
+                     input$exclude_5, input$exclude_6,
+                     input$exclude_7, input$exclude_8,
+                     input$exclude_9)
+        excluir <- (1:9)[excluir]
+        excluir <- as.character(excluir)
+        
+        if(length(excluir) == 0 ) {
+            update_material_button(
+                session,
+                input_id = "clear_exclude",
+                # value = "",
+                icon = "stop",
+                enable = TRUE
+            ) 
+        }
+        else {
+            update_material_button(
+                session,
+                input_id = "clear_exclude",
+                # value = "",
+                icon = "stop",
+                enable = FALSE
+            )
+        }
+        return(excluir)
+    })
+    
+    incluir.m <- reactive({
+        incluir <- c(input$include_1, input$include_2,
+                     input$include_3, input$include_4,
+                     input$include_5, input$include_6,
+                     input$include_7, input$include_8,
+                     input$include_9)
+        incluir <- (1:9)[incluir]
+        incluir <- as.character(incluir)
+        
+        if(length(incluir) == 0 ) {
+            update_material_button(
+                session,
+                input_id = "clear_include",
+                # value = "",
+                icon = "stop",
+                enable = TRUE
+            ) 
+        }
+        else {
+            update_material_button(
+                session,
+                input_id = "clear_include",
+                # value = "",
+                icon = "stop",
+                enable = FALSE
+            )
+        }
+        return(incluir)
+    })
+    
     output$posible.sums <- renderTable(
-        posibles(input$sums,
-                 input$digits,
-                 excluir = input$excluded,
-                 incluir = input$included),
+        {
+            posibles(input$sums,
+                     input$digits, 
+                     excluir = excluir.m(),
+                     incluir = incluir.m())
+        },
         colnames = FALSE,
-        digits = 0)
-
+        digits = 0
+    )
+    
+    # clear_exclude button
+    observe({
+        input$clear_exclude
+        
+        for(i in 1:9) {
+            update_material_checkbox(session, 
+                                     input_id = paste('exclude', i, sep = '_'),
+                                     value = FALSE)
+        }
+    })
+    
+    # clear_include button
+    observe({
+        input$clear_include
+        
+        for(i in 1:9) {
+            update_material_checkbox(session, 
+                                     input_id = paste('include', i, sep = '_'),
+                                     value = FALSE)
+        }
+        
+    })
+    
 })
--- SudokuKillerSums/ui.R
+++ SudokuKillerSums/ui.R
@@ -1,39 +1,250 @@
 
 library(shiny)
-library(markdown)
-library(shinythemes)
+library(shinymaterial)
 
-navbarPage(
-    title = 'Sums of killer Sudoku',
-    # theme = shinytheme("paper"),
-    tabPanel('App',
-             h3('Sums of killer Sudoku'),
-             numericInput("sums",
-                          "Sum needed:",
-                          min = 3, max = 45,
-                          value = 3,
-                          step = 1),
-             numericInput("digits",
-                          "Number of positions:",
-                          min = 2, max = 9,
-                          value = 2,
-                          step = 1),
-
-             checkboxGroupInput("excluded",
-                                "Numbers to exclude:",
-                                1:9, selected = NULL,
-                                inline = TRUE),
-
-             checkboxGroupInput('included',
-                                'Mandatory numbers:',
-                                choices = 1:9,
-                                inline = TRUE),
-
-             h3('Posible combinations:'),
-             tableOutput("posible.sums"),
-             HTML("<a href='http://www.sudokumania.com.ar/juegos/killer'>Play Killer Sudoku!</a>")
-
-             ),
-    tabPanel('Help',  includeMarkdown("help.Rmd"))
+shinyUI(
+    material_page(
+        title = "Sums of killer Sudoku",
+        nav_bar_color = "red darken-4",
+        background_color = "grey lighten-4",
+        
+        # Define tabs
+        material_tabs(
+            tabs = c(
+                "App" = "tab_app",
+                "Help" = "tab_help"
+            ),
+            color = "red" # "deep-orange" #  "pink"  # "blue-grey" #
+        ),
+        
+        # Define tab 1 content
+        material_tab_content(
+            tab_id = "tab_app",
+            
+            material_card(
+                title = "Inputs:",
+                depth = 5,
+                
+                material_row(
+                    material_column(
+                        
+                        material_number_box("sums",
+                                            "Sum needed:",
+                                            min_value = 3, max_value = 45,
+                                            initial_value = 3,
+                                            step_size = 1,
+                                            color = "#b71c1c"),
+                        width = 6),
+                    
+                    material_column(
+                        material_number_box("digits",
+                                            "Number of positions:",
+                                            min_value = 2, max_value = 9,
+                                            initial_value = 2,
+                                            step_size = 1, 
+                                            color = "#b71c1c"
+                        ),
+                        width = 6
+                    )
+                ),
+                
+                p("Numbers to exclude:"),
+                material_row(
+                    material_column(
+                        material_checkbox(
+                            input_id = "exclude_1",
+                            label = "1",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1,
+                        offset = 0
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "exclude_2",
+                            label = "2",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "exclude_3",
+                            label = "3",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "exclude_4",
+                            label = "4",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "exclude_5",
+                            label = "5",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "exclude_6",
+                            label = "6",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "exclude_7",
+                            label = "7",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "exclude_8",
+                            label = "8",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "exclude_9",
+                            label = "9",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_button(
+                            input_id = "clear_exclude",
+                            label = "",
+                            icon = "check_box_outline_blank",
+                            depth = 3,
+                            color = "red lighten-1"
+                        ),
+                        width = 1
+                    )
+                ),
+                
+                p("Mandatory numbers:"),
+                material_row(
+                    material_column(
+                        material_checkbox(
+                            input_id = "include_1",
+                            label = "1",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1,
+                        offset = 0
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "include_2",
+                            label = "2",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "include_3",
+                            label = "3",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "include_4",
+                            label = "4",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "include_5",
+                            label = "5",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "include_6",
+                            label = "6",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "include_7",
+                            label = "7",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "include_8",
+                            label = "8",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_checkbox(
+                            input_id = "include_9",
+                            label = "9",
+                            initial_value = FALSE,
+                            color = "#b71c1c"),
+                        width = 1
+                    ),
+                    material_column(
+                        material_button(
+                            input_id = "clear_include",
+                            label = "",
+                            icon = "check_box_outline_blank",
+                            depth = 3, 
+                            color = "red lighten-1" # "#f05545" # 
+                        ),
+                        width = 1
+                    )
+                )
+                
+            ), 
+            
+            material_card(
+                title = "Posible combinations:",
+                depth = 5,
+                
+                tableOutput("posible.sums")
+                
+            ),
+            
+            material_card(
+                title = "",
+                HTML("<a href='http://www.sudokumania.com.ar/juegos/killer' target='_blank'>Play Killer Sudoku!</a>")
+            )
+            
+            
+        ),
+        
+        # Define tab 2 content
+        material_tab_content(
+            tab_id = "tab_help" ,
+            includeCSS("help_css.css"),
+            includeMarkdown("help.Rmd")
+        )
+    )
 )
-
